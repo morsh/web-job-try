6d859cf00dda110b19188f7dfd8a969e49d16759
morsh <mor.shemesh@gmail.com>
2016-02-09 13:58:49 +0200

    second

--- NodejsWebJob/run.js
+++ NodejsWebJob/run.js
@@ -3,7 +3,7 @@
 console.log('Hello world');
 console.log('Mor was here');
 
-setTimeout(function () {
+setInterval(function () {
     console.info('two seconds have passed...');
     console.info(uuid.v4());
 }, 2000);
