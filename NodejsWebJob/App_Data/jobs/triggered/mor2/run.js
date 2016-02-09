3639d33de0817f90a14b296346c6705ee9e6be45
morsh <mor.shemesh@gmail.com>
2016-02-09 16:28:05 +0200

    three

---
+++ NodejsWebJob/App_Data/jobs/triggered/mor2/run.js
@@ -0,0 +1,9 @@
+﻿var uuid = require('uuid');
+
+console.log('Hello world');
+console.log('Mor was here');
+
+setInterval(function () {
+    console.info('two seconds have passed... WOW!!!!!!!!!!');
+    console.info(uuid.v4());
+}, 2000);
