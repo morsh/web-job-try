8ff8f5db4aa533cf7df7d792330c841b0cf8060d
morsh <mor.shemesh@gmail.com>
2016-02-09 13:51:24 +0200

    first

---
+++ NodejsWebJob/app.js
@@ -0,0 +1,9 @@
+﻿var uuid = require('uuid');
+
+console.log('Hello world');
+console.log('Mor was here');
+
+setTimeout(function () {
+    console.info('two seconds have passed...');
+    console.info(uuid.v4());
+}, 2000);
